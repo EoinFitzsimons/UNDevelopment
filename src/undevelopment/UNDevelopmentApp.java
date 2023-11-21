eeade2de9a426edac240089a9c5f0beb4fc60a91
Conor Judge <conorjudgetech@icloud.com>
2023-11-21 17:54:49 +0000

    print test
    print test

--- src/undevelopment/UNDevelopmentApp.java
+++ src/undevelopment/UNDevelopmentApp.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package undevelopment;
 
 /**
@@ -15,6 +11,7 @@ public class UNDevelopmentApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
